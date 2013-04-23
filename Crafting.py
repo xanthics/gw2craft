5e2012017ed576699093bf4f8d58fd5a601b1191
xanthics <3137623+xanthics@users.noreply.github.com>
2013-04-23 00:07:07 -0700

    Added copyright notice

--- Crafting.py
+++ Crafting.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
 '''
+* Copyright (c) 2013 Jeremy Parks. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+
 Author: Jeremy Parks
 Creation Date: 12/24/2012
 Last Updated: 3/2/2013
-Purpose: Generates a crafting guide	for all crafts in Guild Wars 2 based on current market prices
+Purpose: Generates a crafting guide for all crafts in Guild Wars 2 based on current market prices
+Note: Requires Python 2.7.x and Linux Mint 14(other versions of Linux may work, Windows won't)
 '''
 
 import urllib, json, time, datetime, math, os
