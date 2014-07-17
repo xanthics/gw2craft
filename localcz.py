eee9edc1ac4ffe7b13d1ae42a1ba6845349157e1
DemonioCZ <demosek@gmail.com>
2014-07-17 15:25:45 +0200

    Update and rename localecz.py to localcz.py
    starting with translation to czech language

---
+++ localcz.py
@@ -0,0 +1,287 @@
+# -*- coding: utf-8 -*-
+'''
+* Copyright (c) 2013 Jeremy Parks. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+
+Author: Jeremy Parks
+Purpose: en Localized text
+Note: Requires Python 2.7.x
+'''
+
+region = "pouze NA/EU cenové data"
+
+# Copyright notice for GW2 IP
+cright = u'''<footer>
+    Guild Wars 2 © 2012 ArenaNet, Inc. All rights reserved. NCsoft, the interlocking NC logo, ArenaNet, Guild Wars, Guild Wars Factions, Guild Wars Nightfall, Guild Wars: Eye of the North, Guild Wars 2, and all associated logos and designs are trademarks or registered trademarks of NCsoft Corporation. All other trademarks are the property of their respective owners.
+</footer>'''
+
+# renown heart vendors
+crandle    = u"Agent Crandle - Fort Trinity(Straits of Devastation 70-75)"
+aidem      = u"Aidem Finlay - Hidden Lake(Brisban Wildlands 15-25)"
+albin      = u"Albin Chronicler - The Icesteppes(Wayfarer Foothills 1-15)"
+jack       = u"Apple Jack(16c per) - Cornucopian Fields(Gendarran Fields 25-35)"
+victor     = u"Assistant Chef Victor - Scaver Plateau(Queensdale 1-15)"
+bjarni     = u"Bjarni - Breakneck Pass(Wayfarer Foothills 1-15)"
+braxa      = u"Braxa Scalehunter - Champion's Shield(Iron Marches 50-60)"
+jenks      = u"Deputy Jenks - Giant's Passage (Kessex Hills 15-25)"
+disa       = u"Disa - Snowslide Ravine(Dredgehaunt Cliffs 40-50)"
+drottot    = u"Drottot Lashtail - Devourer's Mouth(Plains of Ashford 1-15)"
+elain      = u"Elain - Grenbrack Delves(Caledon Forest 1-15)"
+jenrys     = u"Environmental Activist Jenrys - Judgement Rock(Mount Maelstrom 60-70)"
+eona       = u"Eona - Mabon Market(Caledon Forest 1-15)"
+makayla    = u"Fallen Angel Makayla - Stronghold of Ebonhawke(Fields of Ruin 30-40)"
+eda        = u"Farmer Eda - Shaemoor Fields(Queensdale 1-15)"
+leius      = u"Field Medic Leius - Nebo Terrace(Gendarran Fields 25-35)"
+glubb      = u"Glubb - Degun Shun(Blazeridge Steppes 40-50)"
+hune       = u"Hune - The Thunderhorns(Lornar's Pass 25-40)"
+ichtaca    = u"Ichtaca - Hunting Banks(Timberline Falls 50-60)"
+kastaz     = u"Kastaz Strongpaw - Noxin Dells(Diessa Plateau 15-25)"
+laewyn     = u"Laewyn - Wychmire Swamp(Caledon Forest 1-15)"
+laudren    = u"Laudren - Thundertroll Swamp(Sparkfly Fen 55-65)"
+pickins    = u"Lieutenant Pickins - Greystone Rise(Harathi Hinterlands 35-45)"
+summers    = u"Lieutenant Summers - Nightguard Beach(Harathi Hinterlands 35-45)"
+auda       = u"Lionguard Auda - Dragon's Rising(Snowden Drifts 15-25)"
+tunnira    = u"Lionscout Tunnira - Archen Foreland(Bloodtide Coast 45-55)"
+kevach     = u"Lodge Keeper Kevach - Dolyak Pass(Wayfarer Foothills 1-15)"
+mcov       = u"Master Craftsman or Vendor"
+maxtar     = u"Maxtar Rapidstep - Dolyak Pass(Wayfarer Foothills 1-15)"
+milton     = u"Milton Book - Cornucopian Fields(Gendarran Fields 25-35)"
+naknar     = u"Naknar - Ebbing Heart Run(Iron Marches 50-60)"
+nrocroc    = u"Nrocroc Chief - Apostate Wastes(Fireheart Rise 60-70)"
+hrouda     = u"PR&T Senior Investigator Hrouda - Akk Wilds(Metrica Province 1-15)"
+pochtecatl = u"Pochtecatl - Jelako Cliffrise(Bloodtide Coast 45-55)"
+hrappa     = u"Researcher Hrappa - Voloxian Passage(Metrica Province 1-15)"
+sagum      = u"Sagum Relicseeker - Agnos Gorge(Plains of Ashford 1-15)"
+sangdo     = u"Sangdo Swiftwing - Cereboth Canyon(Kessex Hills 15-25)"
+tholin     = u"Scholar Tholin - Krongar Pass(Timberline Falls 50-60)"
+vejj       = u"Security Captain Vejj - Almuten Estates(Gendarran Fields 25-35)"
+triktiki   = u"Sentry Triktiki - Arcallion Digs(Harathi Hinterlands 35-45)"
+brian      = u"Seraph Archer Brian - Ossencrest Climb(Snowden Drifts 15-25)"
+goran      = u"Seraph Soldier Goran - The Wendon Steps(Brisban Wildlands 15-25)"
+shelp      = u"Shelp - Degun Shun(Blazeridge Steppes 40-50)"
+vaastas    = u"Vaastas Meatslayer - Village of Butcher's Block(Diessa Plateau 15-25)"
+krug       = u"Veteran Krug - Taminn Foothills(Queensdale 1-15)"
+wupwup     = u"Wupwup Chief - Apostate Wastes(Fireheart Rise 60-70)"
+
+# guide variables
+iCost        = u"Základní cena"
+eRecovery    = u"Očekávaná návratnost"
+fCost        = u"Očekávaná koncová cena"
+sList        = u"Seznam prodejů"
+bRecipes     = u"NÁKUP RECEPTŮ"
+collectibles = u"POTŘEBNÝ MATERIÁL(Zjistěte, jestli nemáte v bance nebo Nakupte na tržišti)"
+warning1     = u"Neobnovujte tuto stránku."
+warning2     = u"Může se změnit. Aktualizováno"
+moreInfo     = u"Kdykoliv uvidíte tuto ikonu %s můžete si rozkliknout více informací."
+soldVia      = u"Prodáváno za %s per via"
+method       = [u"Vendor",
+                u"Max Buyout",
+                u"Minimum Sale Price"]
+valuePer     = u"per"
+buyVendor    = u"BUY VENDOR"
+mixedTP      = u"Mixed (Buy on TP)"
+rTP          = u"Buy on TP"
+make         = u"Make"
+discover     = u"Discover"
+expand       = u"Expand all discovery recipes"
+collapse     = u"Collapse all discovery recipes"
+tier         = u"Tier %i. Levels %i-%i"
+buyList      = u"Buy List(Only Tier %i)"
+blNotice     = u"Notice: If you are following the full guide then you already purchased these materials."
+costRT       = u"Cost: %s (Rolling Total: %s)"
+level        = u"Level"
+finish       = u"Nothing.  You are done!"
+updated      = u"Updated"
+note         = u"<strong>Note:</strong> The prices show here are initial costs and do not take sellback into account."
+craft        = u"Craft"
+tiers        = u"Tier"
+toggle       = u"Click To Toggle"
+kNote        = u"Note: 11 Basil Leaf(e.g.) means buy 1 bulk Basil Leaf and you will have 14 left over"
+bNote        = u"Produces 5 Ingot per make"
+sNote        = u"Produces 2 Soles per make"
+
+# FAQ strings
+costs    = u"Costs are covered, but donations are appreciated"
+gw2spidy = u"Also these guides would not be possible without <a href=\"http://www.gw2spidy.com\">gw2spidy</a> and <a href=\"http://www.guildwarstrade.com/\">guildwarstrade</a>."
+oThread  = u"GW2 Official Forum Discussion Thread"
+rThread  = u"reddit Discussion Thread"
+gThread  = u"guildwars2guru Discussion Thread"
+twitter  = u"Twitter"
+email    = u"Email"
+contact  = u"If you have any questions, comments, concerns or feel that you have noticed a bug please contact me using one of these methods."
+faq      = u"FAQ"
+question = u"Q"
+answer   = u"A"
+source   = u"Source code?"
+q1       = u"How many levels can I get from crafting?"
+a11      = u"The amount of character experience you get from crafting is determined by your progress through the crafting levels based on the following values;"
+a12      = u"1-100 1% of current level per crafting level"
+a13      = u"101-200 2% of current level per crafting level"
+a14      = u"201-300 3% of current level per crafting level"
+a15      = u"301-400 4% of current level per crafting level"
+a16      = u"And these values scale to partial levels, like lvl 1, half full bar is 1.5% of a level. Going 0-400 will give you 10 levels from whatever level you started at, every time."
+a17      = u"There is 1 exception in that xp rewards from 1-15 are larger than the actual xp needed for those levels. ( <a href=\"http://wiki.guildwars2.com/wiki/Experience#Total_experience_gain_per_level\">source</a> ) so you will get about 13 levels while crafting."
+a18      = u"This means you can level from 2-80 entirely from crafting as there are 8 crafts."
+q2       = u"Won't these guides be wrong once everyone buys out item X?"
+a2       = u"These guides suggest crafting or buying items based on current TP prices. They will adjust suggested items as prices change.  (Nearly) all possible recipes are considered when guides are generated."
+q3       = u"Various questions about crit rate/chance or crafting boosters."
+a31      = u"This script assumes a 0% crit rate, so crits may reduce the number of crafts and thus materials needed."
+a32      = u"Crafting boosters give you a +50%, additive with wvw bonus, chance to critical while doing a craft. Max bonus of 70%. What a critical does while crafting is give you 50% bonus crafting experience that is additive with other crafting bonuses(discovery, masterwork and rare crafts). Crafting boosters may make it so you need less crafts to reach 400 in a craft but it will not cause you to gain more than 10 character levels."
+q4       = u"Can I reuse or link to your guides?"
+a4       = u"Yes, just be aware the current output may change and I will not provide support. Also please provide proper attribution ( xanthic.9478 and a link to this site )."
+q5       = u"Various questions about considering TP sell values or adding vendor cost recovery."
+a51      = u"Balancing based on TP sell values would add risk to the guide price, as prices would even out and stagnate removing the ability for some people to recovery costs that way and thus making their run through the guide several silver more expensive. I currently provide a \"best guess\" cost recovery based on maximum bid or vendor price, whichever is greater, and if 0 minimum TP sale price."
+a52      = u"There also doesn't exist an \"automatic\" way to determine if an item will actually sell at the price listed either."
+q6       = u"How are you making these guides?"
+a6       = u"Multi-part Python 2.7.3 script that uses a greedy algorithm and price data from gw2spidy to calculate the lowest initial gold method of leveling a craft using in game formulas"
+q7       = u"No, I mean how are you making these guides? (Simple answer)"
+a7       = u"For each \"make\", I generate a list of items, calculate the highest xp/lowest cost way to make that item based on crafting or buying its parts and then choose the best item to make."
+q8       = u"No, I mean how are you making these guides? (Elaborate answer)"
+a8       = u"First I divide crafting up into 16 25 point buckets(ie 0-24, 25-49).  Then starting at the 375 I compute(for every item that gives xp) its xp/base_cost, and then for every sub part of that item I compute its xp/base_cost and if any of those items have sub parts I then compute their xp/base_cost until I reach items without recipes.  Sub parts are chosen to be made if they are cheaper than buying the part, or they increase the xp/cost ratio more than buying the part.  Which is why the guide may tell you to craft something even if it is slightly cheaper to buy it.  Then after the best choice is found, xp changes are calculated(across all buckets) and then the best choice is found again."
+thanks   = u"Thank you to the people that created guides before me; Qorthos, pwnuniversity, gw2wiz and guildwars-2-crafting. I wouldn't have had the idea to write this without your guides providing a template for me to build from."
+
+# index strings
+fThings = u"4 things you should know"
+t1      = u"<img src=\"/img/arrow.png\" alt=\"ARROW\"> can be clicked for all discovery recipes as well as sold item list"
+t2      = u"<input type=\"checkbox\"/> exist so you can track your location in the buy list"
+t3      = u"Tier specific buy lists exist for non-cooking guides(click the button)"
+t4      = u"<a href=\"nav.html\">Nav page</a> if you can't use the nav bar"
+nge     = u"Will make smart choices between crafting or buying sub parts of an item."
+fge     = u"Makes the same item for 25 points.  Faster and easier than other guides but more expensive."
+tge     = u"Will craft sub parts of an item if possible instead of buying.  Usually more expensive than normal guides, but can be less complex.  This is similar to crafting guides that existed before ACCG."
+wit     = u"<strong>What is this?</strong> Always current crafting guides for Guild Wars 2. All guides are recalculated based on current TP prices every hour assuming the computer running the script is on and gw2spidy is reachable. These guides were originally created for friends, but based on the popularity of these within the Guild Wars 2 community, I continued to improve them to what you see today.  This script does assume there are \"infinite\" of an item at a given cost, so if they all get bought up, or there is very few available during the update window prices may be wrong until the next update."
+nWarn   = u"<strong>[Notice]</strong> If you are not going to do the guide in 1 sitting make sure to save it to your machine. As these guides can and will change."
+rCost   = u"<strong>Hints to reduce costs:</strong> Save a copy of the guide and place buy orders for the materials."
+thanks2 = u"Thank you to Chase, TimeBomb and saladon for some of the CSS styling on these pages.  Thanks to Marc Ramos(desmodium.3745) for the Spanish translation.  And thank you to @figgityfigs for the new icon design(favicon and apple-touch-icon) and hosting"
+
+# nav strings
+navNotice = u"This is a simple nav page, you should only be reaching it if you cannot use the nav bar.  If you are translating this site please email me at gw2crafts@live.com with the source language and translation page used and I will try to fix it."
+navLang   = u"Language"
+
+#nav page headers and guide names
+home    = u"Home"
+nGuides = u"Normal Guides"
+fGuides = u"Fast Guides"
+tGuides = u"Traditional Guides"
+cooking = u"Cooking"
+nHearts = u"No Hearts"
+tHearts = u"Top 5 Hearts"
+aHearts = u"All Hearts"
+jc      = u"Jewelcrafting"
+art     = u"Artificing"
+hunt    = u"Huntsman"
+wc      = u"Weaponcrafting"
+ac      = u"Armorcrafting"
+lw      = u"Leatherworking"
+tailor  = u"Tailoring"
+totals  = u"Totals"
+about   = u"About"
+lang    = u"English"
+
+# directory path
+path = ""
+
+# don't change this
+header = u"""<nav>
+    <ul>
+        <li><a href="http://gw2crafts.net/"""+path+u"""">"""+home+u"""</a></li>
+        <li><a href="#">"""+nGuides+u"""</a>
+        <ul>
+            <li><a href="#">"""+cooking+u"""</a>
+            <ul>
+                <li><a href="http://gw2crafts.net/"""+path+u"""cooking.html">"""+nHearts+u"""</a></li>
+                <li><a href="http://gw2crafts.net/"""+path+u"""cooking_karma_light.html">"""+tHearts+u"""</a></li>
+                <li><a href="http://gw2crafts.net/"""+path+u"""cooking_karma.html">"""+aHearts+u"""</a></li>
+            </ul>
+            </li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""jewelcraft.html">"""+jc+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""artificing.html">"""+art+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""huntsman.html">"""+hunt+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""weaponcraft.html">"""+wc+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""armorcraft.html">"""+ac+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""leatherworking.html">"""+lw+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""tailor.html">"""+tailor+u"""</a></li>
+        </ul>
+        </li>
+        <li><a href="#">"""+fGuides+u"""</a>
+        <ul>
+            <li><a href="#">"""+cooking+u"""</a>
+            <ul>
+                <li><a href="http://gw2crafts.net/"""+path+u"""cooking_fast.html">"""+nHearts+u"""</a></li>
+                <li><a href="http://gw2crafts.net/"""+path+u"""cooking_karma_fast_light.html">"""+tHearts+u"""</a></li>
+                <li><a href="http://gw2crafts.net/"""+path+u"""cooking_karma_fast.html">"""+aHearts+u"""</a></li>
+            </ul>
+            </li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""jewelcraft_fast.html">"""+jc+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""artificing_fast.html">"""+art+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""huntsman_fast.html">"""+hunt+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""weaponcraft_fast.html">"""+wc+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""armorcraft_fast.html">"""+ac+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""leatherworking_fast.html">"""+lw+u"""</a></li>
+            <li><a href="http://gw2crafts.net/"""+path+u"""tailor_fast.html">"""+tailor+u"""</a></li>
+        </ul>
+        </li>
+        <li><a href="#">400+</a>
+        <ul>
+            <li><a href="#">"""+art+u"""</a>
+            <ul>
+                <li><a href="http://gw2crafts.net/"""+path+u"""artificing_450.html">400-450</a></li>
+                <li><a href="http://gw2crafts.net/"""+path+u"""artificing_400.html">400-500</a></li>
+            </ul>
+            <li><a href="#">"""+hunt+u"""</a>
+            <ul>
+                <li><a href="http://gw2crafts.net/"""+path+u"""huntsman_450.html">400-450</a></li>
+                <li><a href="http://gw2crafts.net/"""+path+u"""huntsman_400.html">400-500</a></li>
+            </ul>
+            <li><a href="#">"""+wc+u"""</a>
+            <ul>
+                <li><a href="http://gw2crafts.net/"""+path+u"""weaponcraft_450.html">400-450</a></li>
+                <li><a href="http://gw2crafts.net/"""+path+u"""weaponcraft_400.html">400-500</a></li>
+            </ul>
+            <li><a href="#">"""+ac+u"""</a>
+            <ul>
+                <li><a href="http://gw2crafts.net/"""+path+u"""armorcraft_450.html">400-450</a></li>
+                <li><a href="http://gw2crafts.net/"""+path+u"""armorcraft_400.html">400-500</a></li>
+            </ul>
+            <li><a href="#">"""+lw+u"""</a>
+            <ul>
+                <li><a href="http://gw2crafts.net/"""+path+u"""leatherworking_450.html">400-450</a></li>
+                <li><a href="http://gw2crafts.net/"""+path+u"""leatherworking_400.html">400-500</a></li>
+            </ul>
+            <li><a href="#">"""+tailor+u"""</a>
+            <ul>
+                <li><a href="http://gw2crafts.net/"""+path+u"""tailor_450.html">400-450</a></li>
+                <li><a href="http://gw2crafts.net/"""+path+u"""tailor_400.html">400-500</a></li>
+            </ul>
+        </ul>
+        </li>
+        <li><a href="http://gw2crafts.net/"""+path+u"""total.html">"""+totals+u"""</a></li>
+        <li><a href="http://gw2crafts.net/"""+path+u"""faq.html">"""+about+u"""</a></li>
+        <li><a href="#" class="language" hreflang="en">"""+lang+u"""</a>
+        <ul>
+          <li><a href="#" hreflang="en">English</a></li>
+          <li><a href="http://gw2crafts.net/fr/%s" hreflang="fr">Français</a></li>
+          <li><a href="http://gw2crafts.net/de/%s" hreflang="de">Deutsch</a></li>
+          <li><a href="http://gw2crafts.net/es/%s" hreflang="es">Español</a></li>
+        </ul>
+        </li>
+    </ul>
+</nav>
+"""
