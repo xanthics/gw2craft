096c37bc8dc6f1660eb632fcfb794e1cfc131091
xanthics <xanthic42@gmail.com>
2016-07-30 13:51:05 -0700

    updated to boto3

--- output_aws.py
+++ output_aws.py
@@ -28,11 +28,8 @@ Note: Requires Python 2.7.x
 import os
 import time
 from random import randint
-from StringIO import StringIO
 
-import boto
-import boto.s3
-from boto.s3.key import Key
+import boto3
 
 # Credentials
 from Ftp_info import amakey, amasec
@@ -41,14 +38,10 @@ from Ftp_info import amakey, amasec
 def write_file(directory, filename, contents):
 	while True:
 		try:
-			keyname = os.path.join( '{}'.format(directory), filename)
-			conn = boto.connect_s3(amakey, amasec)
-			bucket = conn.get_bucket('gw2crafts.net')
-			k = Key(bucket)
-			k.key = keyname
-			k.metadata.update({'Content-Type':'text/html'})
-			k.set_contents_from_string(contents.encode('utf8'))
+			keyname = os.path.join('{}'.format(directory), filename)
+			s3 = boto3.resource('s3')
+			s3.Object('gw2crafts.net', keyname).put(Body=contents.encode('utf8'), ContentType='text/html')
 			return
 		except Exception, err:
 			print u'ERROR: %s.' % str(err)
-			time.sleep(randint(1,10))
+			time.sleep(randint(1, 10))
