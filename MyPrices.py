dc0a0c3641f2bac43ef144028d1d531a55ce5d25
xanthics <3137623+xanthics@users.noreply.github.com>
2016-08-20 14:54:08 -0700

    Changed to urllib2 to fix socket timeout issues.

--- MyPrices.py
+++ MyPrices.py
@@ -27,30 +27,17 @@ Note: Requires Python 2.7.x
 '''
 
 import json
-import math
 import os
 import sys
 import time
 
-from hashlib import md5
 from auto_gen import Items
 # so we can set custom headers
-from multiprocessing import Process, Queue, cpu_count, Pool
+from multiprocessing import Pool
 from urllib import FancyURLopener
 import urllib2
 from random import randint, choice
 import socket
-
-
-# pretend we are a browser
-class MyOpener(FancyURLopener):
-	user_agents = [
-		'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-		'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36',
-		'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
-		'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'
-	]
-	version = choice(user_agents)
 
 
 def gw2apilistworker(input):
